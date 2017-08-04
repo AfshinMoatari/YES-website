a31df134fd91d8a6f85ffe88f0ae3dc38228057d
Afshin Moatari <Afshinmoatari@user.noreply.github.com>
2017-08-04 15:52:13 +0200

    update the quotation slider

--- js/my.js
+++ js/my.js
@@ -1,9 +1,33 @@
 'use strict';
 
 $(function() {
+  $('.my-slider').slick({
+    infinite: true,
+    arrows: true,
+    speed: 2300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    responsive: [{
+        breakpoint: 960,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
   $(".bars li .bar").each(function(key, bar) {
-    var percentage = $(this).data('percentage');
-
+    let percentage = $(this).data('percentage');
     $(this).animate({
       'height': percentage + '%'
     }, 1000);
