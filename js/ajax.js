773e2600e0606df311c1f480c3842e6df3b139a1
Afshin Moatari <Afshinmoatari@user.noreply.github.com>
2017-08-21 05:51:19 +0200

    init

--- js/ajax.js
+++ js/ajax.js
@@ -1,30 +1,18 @@
 $(function() {
-  // Get the form.
   let form = $('#company form');
-  // Get the messages div.
   let formMessages = $('#form-messages');
-
-  // Set up an event listener for the contact form.
   $(form).submit(function(event) {
-    // Stop the browser from submitting the form.
     event.preventDefault();
-    // Serialize the form data.
-    var formData = $(form).serialize();
-    // Submit the form using AJAX.
+    let formData = $(form).serialize();
     $.ajax({
         type: 'POST',
         url: $(form).attr('action'),
         data: formData
       })
       .done(function(response) {
-        // Make sure that the formMessages div has the 'success' class.
         $(formMessages).removeClass('error');
         $(formMessages).addClass('success');
-
-        // Set the message text.
         $(formMessages).text(response);
-
-        // Clear the form.
         $('#name').val('');
         $('#behalf').val('');
         $('#message').val('');
@@ -33,11 +21,8 @@ $(function() {
         $('#time').val('');
       })
       .fail(function(data) {
-        // Make sure that the formMessages div has the 'error' class.
         $(formMessages).removeClass('success');
         $(formMessages).addClass('error');
-
-        // Set the message text.
         if (data.responseText !== '') {
           $(formMessages).text(data.responseText);
         } else {
